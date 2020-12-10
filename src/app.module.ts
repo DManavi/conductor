91c3050b50cc32736b88643845226e7c44f628df
Danial Manavi <dmanavi@live.com>
2020-12-10 08:19:24 +0330

    Add health to the app module.

--- src/app.module.ts
+++ src/app.module.ts
@@ -7,9 +7,10 @@ import { CommunicationModule } from './communication/communication.module';
 import { WorkflowStateModule } from './workflow-state/workflow-state.module';
 import { WorkflowStorageModule } from './workflow-storage/workflow-storage.module';
 import { LoggerModule } from './logger/logger.module';
+import { HealthModule } from './health/health.module';
 
 @Module({
-    imports: [CommunicationModule, WorkflowStateModule, WorkflowStorageModule, LoggerModule],
+    imports: [CommunicationModule, WorkflowStateModule, WorkflowStorageModule, LoggerModule, HealthModule],
     controllers: [],
     providers: [],
 })
